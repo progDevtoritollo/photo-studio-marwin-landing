ea1ad1c2f5444ddbe862060a9f6f65e6db7b7164
Igor Marusych <progdevtoritollo@gmail.com>
2023-04-15 13:14:34 +0300

    config custom typography

--- src/config/theme/index.ts
+++ src/config/theme/index.ts
@@ -1,22 +1,176 @@
-import { createTheme as createMuiTheme, Theme } from '@mui/material/styles'
+import { createTheme  } from '@mui/material/styles'
 
-import typography from './typography'
+// import typography from './typography'
 import paletteBase from './palette-base'
 import paletteLight from './palette-light'
 import paletteDark from './palette-dark'
 import shadows from './shadows'
 
-// default
-const createTheme = (darkMode?: boolean): Theme => {
-  const palette = darkMode ? { ...paletteBase, ...paletteDark } : { ...paletteBase, ...paletteLight }
-  return createMuiTheme({
-    palette,
-    typography,
-    shadows,
-  })
+
+
+declare module '@mui/material/styles' {
+  interface TypographyVariants {
+    logo: React.CSSProperties;
+    
+    Heading1: React.CSSProperties;
+    Heading2: React.CSSProperties;
+    Heading3: React.CSSProperties;
+    Heading4: React.CSSProperties;
+    Heading4_with_line: React.CSSProperties;
+    Heading5: React.CSSProperties;
+
+    Paragraph1_light: React.CSSProperties;
+    Paragraph1_regular: React.CSSProperties;
+    Paragraph2: React.CSSProperties;
+    Paragraph3: React.CSSProperties;
+
+  }
+
+  // allow configuration using `createTheme`
+  interface TypographyVariantsOptions {
+    logo?: React.CSSProperties;
+
+    Heading1?: React.CSSProperties;
+    Heading2?: React.CSSProperties;
+    Heading3?: React.CSSProperties;
+    Heading4?: React.CSSProperties;
+    Heading4_with_line?: React.CSSProperties;
+    Heading5?: React.CSSProperties;
+
+    Paragraph1_light?: React.CSSProperties;
+    Paragraph1_regular?: React.CSSProperties;
+    Paragraph2?: React.CSSProperties;
+    Paragraph3?: React.CSSProperties;
+  }
 }
 
-const theme = createTheme(false)
+// Update the Typography's variant prop options
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    logo: true;
 
-export { paletteBase, paletteLight, paletteDark, typography, shadows }
+    Heading1:true;
+    Heading2:true
+    Heading3:true
+    Heading4:true
+    Heading4_with_line:true
+    Heading5:true
+
+    Paragraph1_light: true
+    Paragraph1_regular: true
+    Paragraph2:true
+    Paragraph3:true
+
+
+  }
+}
+  //400 - regular,500 - medium, 700 - bold, 
+const theme = createTheme({
+  spacing: 10, 
+  palette:{ ...paletteBase, ...paletteLight, },
+  typography: {
+    logo: {
+      fontSize: '40px',
+      color: '#000000',
+      fontFamily: 'Vogue',
+      fontStyle: 'normal',
+      fontWeight: 400, 
+      lineHeight: 36,
+    },
+    Heading1:{
+      color: '#000000',
+      fontFamily: 'Dela Gothic One',
+      fontStyle: 'normal',
+      fontWeight: 400,  
+      fontSize: '58px',
+      lineHeight: '115%',
+    },Heading2:{
+      fontSize: '48px',
+      color: '#000000',
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '58px',
+    fontFeatureSettings: '"pnum" on, "lnum" on',
+
+    },Heading3:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 700, //bolt
+      fontSize: '30px',
+      lineHeight: '36px',
+      fontFeatureSettings: '"pnum" on, "lnum" on',
+      color: '#000000',
+    },
+    Heading4:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '25px',
+      lineHeight: '30px',
+      fontFeatureSettings: '"pnum" on, "lnum" on',
+      color: '#000000',
+    },
+    Heading4_with_line:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 700,
+      fontSize: '25px',
+      lineHeight: '30px',
+      textDecorationLine: 'underline',
+      color: '#000000',
+    },
+    Heading5:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      fontSize: '16px',
+      lineHeight: '19px',
+      color: '#000000',
+    },
+    Paragraph1_light:{
+      fontFamily: 'Corbel',
+    fontStyle: 'normal',
+    fontWeight: 300,
+    fontSize: '25px',
+    lineHeight: '30px',
+    color: '#000000',
+    },
+    Paragraph1_regular:{
+      fontFamily: 'Corbel',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    fontSize: '25px',
+    lineHeight: '30px',
+    fontFeatureSettings: "'pnum' on, 'lnum' on",
+    color: '#000000',
+    },
+    Paragraph2:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 300,
+      fontSize: '18px',
+      lineHeight: '22px',
+      color: '#000000',
+    },
+    Paragraph3:{
+      fontFamily: 'Corbel',
+      fontStyle: 'normal',
+      fontWeight: 300,
+      fontSize: '14px',
+      lineHeight: '17px',
+      color: '#000000',
+    },
+    fontFamily: [
+      'Corbel',
+      'Dela Gothic One',
+      'Corbel',
+    ].join(','),
+  },
+  shadows,
+})
+
+
+
+export { paletteBase, paletteLight, paletteDark, /*{typography,}*/ shadows }
 export default theme
--- src/config/theme/typography.ts
+++ src/config/theme/typography.ts
@@ -1,59 +1,90 @@
-import { TypographyOptions } from '@mui/material/styles/createTypography'
+// import { TypographyOptions, TypographyStyleOptions } from '@mui/material/styles/createTypography';
 
-export const fontFamily = [
-  '"Cabin",-apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
-].join(',')
 
-const headingLineHeight = 1.4
+// export const fontFamily = [
+//   '"Cabin",-apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+// ].join(',')
 
-const typography: TypographyOptions = {
-  fontFamily,
-  fontWeightLight: 400,
-  fontWeightRegular: 400,
-  fontWeightMedium: 600,
-  fontWeightBold: 700,
-  h1: {
-    fontSize: 34,
-    lineHeight: headingLineHeight,
-    fontWeight: 700,
-  },
-  h2: {
-    fontSize: 28,
-    lineHeight: headingLineHeight,
-    fontWeight: 700,
-  },
-  h3: {
-    fontSize: 24,
-    lineHeight: headingLineHeight,
-    fontWeight: 700,
-  },
-  h4: {
-    fontSize: 22,
-    lineHeight: headingLineHeight,
-    fontWeight: 700,
-  },
-  h5: {
-    fontSize: 17,
-    lineHeight: headingLineHeight,
-    fontWeight: 600,
-  },
-  h6: {
-    fontSize: 15,
-    lineHeight: headingLineHeight,
-    fontWeight: 600,
-  },
-  body1: {
-    fontSize: '1rem',
-  },
-  body2: {
-    fontSize: '0.9rem',
-  },
-  subtitle1: {
-    fontSize: '0.85rem',
-  },
-  subtitle2: {
-    fontSize: '0.8rem',
-  },
-}
+// const headingLineHeight = 1.4
 
-export default typography
+
+
+
+
+// const typography: TypographyStyleOptions   = {
+//   typography: {
+//     poster: {
+//       fontSize: 64,
+//       color: 'red',
+//     },
+//     h1: {
+//       fontSize: 34,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 700,
+//     },
+//     h2: {
+//       fontSize: 28,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 700,
+//     },
+//     h3: {
+//       fontSize: 24,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 700,
+//     },
+//     h4: {
+//       fontSize: 22,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 700,
+//     },
+//     h5: {
+//       fontSize: 17,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 600,
+//     },
+//     h6: {
+//       fontFamily: 'Helvetica Neue',
+//       fontSize: 15,
+//       lineHeight: headingLineHeight,
+//       fontWeight: 600,
+//     },
+//     body1: {
+//       fontSize: '1rem',
+//     },
+//     body2: {
+//       fontSize: '0.9rem',
+//     },
+//     subtitle1: {
+//       fontSize: '0.85rem',
+//     },
+//     subtitle2: {
+//       fontSize: '0.8rem',
+//     },
+//     // Logo: {
+//     //   fontSize: 16,
+//     //   lineHeight: 1.2,
+//     //   fontWeight: 500,
+//     // },
+//   },
+//   components: {
+//     MuiTypography: {
+//       defaultProps: {
+//         variantMapping: {
+//           // Map the new variant to render a <h1> by default
+//           // poster: 'h1',
+//           // Logo:'h3'
+//         },
+//       },
+//     },
+//   },
+
+//   fontFamily,
+//   fontWeightLight: 400,
+//   fontWeightRegular: 400,
+//   fontWeightMedium: 600,
+//   fontWeightBold: 700,
+
+  
+// }
+
+// export default typography;
