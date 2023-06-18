4bcfa785f01756f7facde105f569964ec0865070
Igor Marusych <progdevtoritollo@gmail.com>
2023-06-18 14:36:13 +0300

    add transition to '/'

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from 'react'
 import { Box, Typography } from '@mui/material'
+import Link from 'next/link';
+
 
 const Logo: FC = () => {
   return (
     <Box
     >
-      <Typography
-        variant="logo"
-        sx={{
-          fontSize: { xs: 24, sm: 25, md: 40 }
-        }}
-      >
-        MARWIN
-      </Typography>
+      <Link href='/' legacyBehavior>
+        <a style={{ textDecoration: 'none' }}>
+          <Typography
+            variant="logo"
+            sx={{
+              fontSize: { xs: 24, sm: 25, md: 40 }
+            }}
+          >
+            MARWIN
+          </Typography>
+        </a>
+      </Link>
     </Box>
   )
 }
